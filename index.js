f96d42d8c27dff507da743f17da34582cebcc58f
Carlos De Los Reyes <cadelosreyest@gmail.com>
2020-03-12 21:21:40 -0500

    first try

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const core = require('@actions/core');
 const wait = require('./wait');
-
+const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+	try {
+		const ms = core.getInput('milliseconds');
+		const context = github.context;
+		await exec.exec(`echo ${JSON.stringify(context)}`);
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+		core.debug(new Date().toTimeString());
+		await wait(parseInt(ms));
+		core.debug(new Date().toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
+		core.setOutput('time', new Date().toTimeString());
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 
-run()
+run();
