b3b5a381c56483b8631b7c45a8875ed04a9ba37d
Carlos De Los Reyes <cadelosreyest@gmail.com>
2020-03-12 22:12:15 -0500

    change the path of the file

--- dist/index.js
+++ dist/index.js
@@ -1392,7 +1392,7 @@ async function run() {
 		await exec.exec(`echo ${JSON.stringify(context.repo.repo)}`);
 		await exec.exec(`npm i -g sbxcloud-cli`);
 		await exec.exec(
-			`sbxcloud-cli deploy README.md ${core.getInput('folder')} ${core.getInput(
+			`sbxcloud-cli deploy ../README.md ${core.getInput('folder')} ${core.getInput(
 				'domain'
 			)} --username=${core.getInput('username')} --password=${core.getInput(
 				'password'
